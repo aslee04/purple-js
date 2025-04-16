7b306fea5e39df4ee4476185a83c1d1f53621266
aslee04 <mukhammadiev05@gmail.com>
2025-04-16 14:08:26 +0500

    did one exercise

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log('Hello World!');
+
+let payRateUsd = 80
+const projectHours = 40
+const workHours = 9 * 5
+console.log('iwni qabul qila olamanmi: ' + (workHours > projectHours))
+console.log('iw ucun summa: ' + (projectHours * payRateUsd))
