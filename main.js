72807fec5c10202e62d2de4a8896132a3ca73476
aslee04 <mukhammadiev05@gmail.comm>
2025-04-17 13:55:54 +0500

    did anither one exercise switch case

--- main.js
+++ main.js
@@ -34,3 +34,72 @@ if (result >= house) {
 } else {
     console.log(`iwlagan pulim: ${result}$. Uyga pulim yetmayapti blyat suka. Menda bor yog'i ${result}$ pulim bor. Kallanga ewwak sikkur.`)
 }
+
+const role = 'nutiti'
+
+if (role === 'manager') {
+    console.log('Manager')
+} else if (role === 'admin') {
+    console.log('Admin')
+} else if (role === 'ceo') {
+    console.log('CEO')
+} else {
+    console.log('miy tebya ne znayim')
+}
+
+switch (role) {
+    case 'manager': 
+        console.log('Manager')
+        break
+    case 'admin':
+        console.log('Admin')
+        break
+    case 'ceo':
+        console.log('CEO')
+        break
+    default:
+        console.log('Miy tebya ne znayiim')
+}
+
+let num = 0
+
+switch (true) {
+    case num > 0:
+        console.log('Musbat son')
+        break
+    case num < 0:
+        console.log('Manfiy')
+        break
+    default: 
+    console.log('bu sonni kotinga tiq')
+}
+
+const bmw = 14000
+const mustang = 18000
+const budget = 123300002
+
+let message
+
+if (budget > bmw) {
+    message = 'bmw olaman'
+} else if (budget > bmw, budget > mustang) {
+    message = 'ikkalasiniyam olaman'
+} else {
+    message = 'piwkom yur bich'
+}
+
+console.log(message)
+
+const user = prompt('Сколько будет 7 + или - 15?')
+
+switch (true) {
+    case user > 0:
+        console.log('Viy ne robot:' + ' Uspex')
+        break
+    case user < 0:
+        console.log('Viy robot:' + ' Ne uspex')
+        break
+    default:
+        console.log('viy nekto')
+        break
+}
